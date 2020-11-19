04829d66a02baba9b99276903c61940194ac93da
Ralph114 <simek.r03@htlwienwest.at>
2020-11-19 08:58:26 +0100

    flipp works

---
+++ v1.js
@@ -0,0 +1,19 @@
+const numberCards=16
+
+let c=document.querySelectorAll('.deck')
+for (let index = 0; index < numberCards; index++) {
+    let c=document.createElement('div')
+    c.innerHTML=index % 3 + 1
+    c.className="card"
+    c.addEventListener('click',flipp)
+    p.appendChild(c)
+    
+}
+
+
+
+
+function flipp(event){
+    this.classList.toggle('flipped')
+ 
+}
